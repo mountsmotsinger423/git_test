1d8f36d2e5cdfcad452ea3bbd36184825b934db5
mountsmotsinger423 <mountsmotsinger423@gmail.com>
2025-03-11 11:20:54 +0000

    Add 6.js

---
+++ src/6.js
@@ -0,0 +1,8 @@
+function getRandomJSCode() {
+  const possibleCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let code = '';
+  for (let i = 0; i < 10; i++) {
+    code += possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+  }
+  return code;
+}
