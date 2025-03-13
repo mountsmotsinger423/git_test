f3b1a0093090200ce21c28977abbbb527160ac42
mountsmotsinger423 <mountsmotsinger423@gmail.com>
2025-03-13 23:57:12 +0000

    Add 8.js

---
+++ src/8.js
@@ -0,0 +1,7 @@
+// Randomly generated JS code
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+const randomNumber = getRandomInt(10);
+console.log(`The randomly generated number is: ${randomNumber}`);
